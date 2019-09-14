f7cd046a5938c1324e0f2d4844084c65efe157f9
Joel Worrall <jworrall@newrelic.com>
2019-09-14 11:25:56 -0400

    fix(TopNerdlet): handling breaking changes in the SDK.

---
+++ nerdlets/top-nerdlet/root.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { EntityByGuidQuery, Spinner } from 'nr1';
+
+import Top from './top';
+
+export default class TopNerdlet extends React.Component {
+  static propTypes = {
+    launcherUrlState: PropTypes.object.isRequired,
+    nerdletUrlState: PropTypes.object.isRequired,
+  };
+
+  render() {
+    const { entityGuid } = this.props.nerdletUrlState;
+    return (
+      <EntityByGuidQuery entityGuid={entityGuid}>
+        {({ loading, error, data }) => {
+          if (loading) return <div />;
+          const entity = data.entities[0];
+          return entity ? <Top entity={entity} {...this.props} /> : <div />;
+        }}
+      </EntityByGuidQuery>
+    );
+  }
+}
