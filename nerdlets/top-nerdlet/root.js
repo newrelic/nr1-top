3bf447cd8baba42a26d60bf591ff9b561838ee92
Michael Frederick <mfrederick@newrelic.com>
2020-01-31 12:36:56 -0500

    fix: spelling (#19)

--- nerdlets/top-nerdlet/root.js
+++ nerdlets/top-nerdlet/root.js
@@ -25,7 +25,7 @@ export default class TopNerdlet extends React.Component {
               <div className="empty-state-container">
                 <EmptyState
                   heading="No entity found"
-                  description="This Nerdpack must be run on a monitored host with the New Relic Infrastructure agent delployed on it. Please select a host that meets that criteria."
+                  description="This Nerdpack must be run on a monitored host with the New Relic Infrastructure agent deployed on it. Please select a host that meets that criteria."
                   buttonText=""
                 ></EmptyState>
               </div>
