e1a55464ac26ae49e75c3346850f6f8676a2178c
Anna Strom-Olsen <aso1124@gmail.com>
2023-02-07 17:30:40 -0500

    feat: add HelpModal component
    Add HelpModal to provide easy links to various github repo locations.

--- nerdlets/top-nerdlet/index.js
+++ nerdlets/top-nerdlet/index.js
@@ -1,45 +1,95 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   EmptyState,
   EntityByGuidQuery,
   NerdletStateContext,
-  Spinner
+  Spinner,
+  nerdlet,
+  Icon
 } from 'nr1';
+import { HelpModal } from '@newrelic/nr-labs-components';
 import Top from './top';
 
 const TopNerdlet = () => {
+  // state that controls the open/closed state of the modal
+  const [helpModalOpen, setHelpModalOpen] = useState(false);
+
+  // effect to configure the Help button in the nerdlet action bar
+  useEffect(() => {
+    nerdlet.setConfig({
+      actionControls: true,
+      actionControlButtons: [
+        {
+          label: 'Help',
+          hint: 'Quick links to get support',
+          type: 'primary',
+          iconType: Icon.TYPE.INTERFACE__INFO__HELP,
+          onClick: () => setHelpModalOpen(true)
+        }
+      ]
+    });
+  }, []);
+
   return (
-    <NerdletStateContext.Consumer>
-      {({ entityGuid }) => (
-        <EntityByGuidQuery entityGuid={entityGuid}>
-          {({ loading, data, error }) => {
-            if (loading) {
-              return <Spinner />;
+    <>
+      <NerdletStateContext.Consumer>
+        {({ entityGuid }) => (
+          <EntityByGuidQuery entityGuid={entityGuid}>
+            {({ loading, data, error }) => {
+              if (loading) {
+                return <Spinner />;
+              }
+              if (error) {
+                throw new Error(error);
+              }
+              const entity = data.entities[0];
+              if (entity) {
+                return <Top entity={entity} />;
+              } else {
+                return (
+                  <EmptyState
+                    fullHeight
+                    fullWidth
+                    iconType={
+                      EmptyState.ICON_TYPE
+                        .HARDWARE_AND_SOFTWARE__SOFTWARE__ALL_ENTITIES
+                    }
+                    title="No entity found"
+                    description="This nerdpack is not enabled for the selected account - please choose an entity from an enabled account, or ask your New Relic admin to enable this nerdpack for this account."
+                  />
+                );
+              }
+            }}
+          </EntityByGuidQuery>
+        )}
+      </NerdletStateContext.Consumer>
+      <HelpModal
+        isModalOpen={helpModalOpen}
+        setModalOpen={setHelpModalOpen}
+        urls={{
+          docs: 'https://github.com/newrelic/nr1-top#readme',
+          createIssue:
+            'https://github.com/newrelic/nr1-top/issues/new?assignees=&labels=bug%2C+needs-triage&template=bug_report.md&title=',
+          createFeature:
+            'https://github.com/newrelic/nr1-top/issues/new?assignees=&labels=enhancement%2C+needs-triage&template=enhancement.md&title=',
+          createQuestion:
+            'https://github.com/newrelic/nr1-top/discussions/new/choose'
+        }}
+        ownerBadge={{
+          logo: {
+            src: 'https://drive.google.com/uc?id=1BdXVy2X34rufvG4_1BYb9czhLRlGlgsT',
+            alt: 'New Relic Labs'
+          },
+          blurb: {
+            text: 'This a New Relic Labs open source app.',
+            link: {
+              text: 'Take a look at our other repos',
+              url: 'https://github.com/newrelic?q=nrlabs-viz&type=all&language=&sort='
             }
-            if (error) {
-              throw new Error(error);
-            }
-            const entity = data.entities[0];
-            if (entity) {
-              return <Top entity={entity} />;
-            } else {
-              return (
-                <EmptyState
-                  fullHeight
-                  fullWidth
-                  iconType={
-                    EmptyState.ICON_TYPE
-                      .HARDWARE_AND_SOFTWARE__SOFTWARE__ALL_ENTITIES
-                  }
-                  title="No entity found"
-                  description="This nerdpack is not enabled for the selected account - please choose an entity from an enabled account, or ask your New Relic admin to enable this nerdpack for this account."
-                />
-              );
-            }
-          }}
-        </EntityByGuidQuery>
-      )}
-    </NerdletStateContext.Consumer>
+          }
+        }}
+      />
+    </>
   );
 };
 
